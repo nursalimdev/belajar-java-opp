886b7f835623d0247792aaf7c6b6223ff3ffab96
nursalimdev <nursalimdev123@gmail.com>
2023-01-28 12:34:22 +0700

    commit

---
+++ src/mypackage/app/AnnotationApp.java
@@ -0,0 +1,10 @@
+package mypackage.app;
+
+import mypackage.annotation.Fancy;
+
+@Fancy(name = "fancy", tags = {"java", "application"})
+public class AnnotationApp {
+    public static void main(String[] args) {
+
+    }
+}
